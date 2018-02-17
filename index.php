85427b3c744dbb30c0e3c91124159a8ec3856f23
vicente olivares <vicenteoli93@gmail.com>
2018-02-17 00:47:47 -0600

    Estructura del Proyecto 25%

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+ include '../scripts/conexion.php';
+ include '../assets/header.php';
+?>
+
+<h1 class='text-white'>INICIO DE SESIÓN</h1>
+
+<?php include '../assets/footer.php'; ?>
---
+++ scripts/conexion.php
@@ -0,0 +1,3 @@
+<?php
+$con = new PDO("mysql:host=127.0.0.1; port:3307; dbname=basic_clothes;","root","root");
+ ?>
